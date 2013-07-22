5655d2fbf71e413c112d566cbce7f716d645d1e6
Dave W <drait@nebachadnezzar>
2013-07-22 13:18:26 -0700

    initial 2.0 testing

---
+++ ATTStatusbarIcons/src/com/dr8/sbicons/mod/ZipStuff.java
@@ -0,0 +1,39 @@
+package com.dr8.sbicons.mod;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+public class ZipStuff {
+
+	private static String TAG = "XSBM";
+	
+	public static Bitmap getBitmapFromZip(final String zipFilePath, final String imageFileInZip){
+	    Log.i(TAG, "Getting image '" + imageFileInZip + "' from '" + zipFilePath +"'");
+	    Bitmap result = null;
+	    try {
+	        FileInputStream fis = new FileInputStream(zipFilePath);
+	        ZipInputStream zis = new ZipInputStream(fis);
+	        ZipEntry ze = null;
+	        while ((ze = zis.getNextEntry()) != null) {
+	            if (ze.getName().equals(imageFileInZip)) {
+	                result = BitmapFactory.decodeStream(zis);
+	                break;
+	            }
+	        }
+	    } catch (FileNotFoundException e) {
+	        Log.i(TAG, "Extracting file: Error opening zip file - FileNotFoundException: ", e);
+	    } catch (IOException e) {
+	        Log.i(TAG, "Extracting file: Error opening zip file - IOException: ", e);
+	        e.printStackTrace();
+	    }
+	    return result;
+	}
+
+}
--- ATTStatusbarIcons/src/com/dr8/sbicons/mod/hax/Bluetooth.java
+++ ATTStatusbarIcons/src/com/dr8/sbicons/mod/hax/Bluetooth.java
@@ -1,8 +1,14 @@
 package com.dr8.sbicons.mod.hax;
 
 import android.content.res.XModuleResources;
+import android.content.res.XResources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Environment;
 
 import com.dr8.sbicons.R;
+import com.dr8.sbicons.mod.ZipStuff;
 
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
@@ -13,6 +19,17 @@ public class Bluetooth {
 	public static void initPackageResources(XSharedPreferences paramPrefs, XModuleResources modRes, XC_InitPackageResources.InitPackageResourcesParam resParam) {
 		try {
 			String targetpkg = "com.android.systemui";
+			String path = Environment.getExternalStorageDirectory() + "/xsbm/test.zip";
+			String iname = "stat_sys_data_bluetooth.png";
+			final Bitmap b = ZipStuff.getBitmapFromZip(path, iname);
+			if (b != null) {
+				resParam.res.setReplacement(targetpkg , "drawable", "stat_sys_data_bluetooth", new XResources.DrawableLoader() {
+					@Override
+					public Drawable newDrawable(XResources res, int id) throws Throwable {
+						return new BitmapDrawable(b);
+					}
+				});
+			}
 			resParam.res.setReplacement(targetpkg , "drawable", "stat_sys_data_bluetooth", modRes.fwd(R.drawable.stat_sys_data_bluetooth));
 			resParam.res.setReplacement(targetpkg, "drawable", "stat_sys_data_bluetooth_connected", modRes.fwd(R.drawable.stat_sys_data_bluetooth_connected));
 		} catch (Throwable t) { XposedBridge.log(t); }
