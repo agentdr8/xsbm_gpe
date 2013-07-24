11c5b4a2514ee9f174f495674c654bfc164754fe
Dave W <drait@nebachadnezzar>
2013-07-24 00:38:29 -0700

    add the auto-hide mobile data pref back in

--- ATTStatusbarIcons/src/com/dr8/sbicons/mod/hax/HtcNetworkController.java
+++ ATTStatusbarIcons/src/com/dr8/sbicons/mod/hax/HtcNetworkController.java
@@ -8,40 +8,13 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class HtcNetworkController {
 
 	public static void initHandleLoadPackage(final XSharedPreferences paramPrefs, XC_LoadPackage.LoadPackageParam lpParam) {
-		if (paramPrefs.getString("iconset", "att").equals("att") && (!paramPrefs.getBoolean("hideatt", false))) {
-			findAndHookMethod("com.android.systemui.statusbar.policy.HtcGenericNetworkController", lpParam.classLoader, "isATT", new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(MethodHookParam attbool) throws Throwable {
-					attbool.setResult(true);
-				}
-			});
-		} else if (paramPrefs.getString("iconset", "att").equals("att") && (paramPrefs.getBoolean("hideatt", true))) {
+		if (paramPrefs.getBoolean("hideatt", false)) {
 			findAndHookMethod("com.android.systemui.statusbar.policy.HtcGenericNetworkController", lpParam.classLoader, "isATT", new XC_MethodHook() {
 				@Override
 				protected void afterHookedMethod(MethodHookParam attbool) throws Throwable {
 					attbool.setResult(false);
 				}
 			});
-		} else if (paramPrefs.getString("iconset", "att").equals("tmobus")) {
-			findAndHookMethod("com.android.systemui.statusbar.policy.HtcGenericNetworkController", lpParam.classLoader, "isATT", new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(MethodHookParam attbool) throws Throwable {
-					attbool.setResult(false);
-				}
-			});
-			findAndHookMethod("com.android.systemui.statusbar.policy.HtcGenericNetworkController", lpParam.classLoader, "isTmoUS", new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(MethodHookParam tmobool) throws Throwable {
-					tmobool.setResult(true);
-				}
-			});
-		} else {
-			findAndHookMethod("com.android.systemui.statusbar.policy.HtcGenericNetworkController", lpParam.classLoader, "isATT", new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(MethodHookParam attbool) throws Throwable {
-					attbool.setResult(false);
-				}
-			});
-		}
+		} 
 	}
 }
