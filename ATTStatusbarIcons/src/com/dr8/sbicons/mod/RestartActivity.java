39ffabdd0a8e33fb24200e426881cbb5eab735ef
Dave W <drait@nebachadnezzar>
2013-07-31 23:21:18 -0700

    added restart option, updated default iconpack

--- ATTStatusbarIcons/src/com/dr8/sbicons/mod/RestartActivity.java
+++ ATTStatusbarIcons/src/com/dr8/sbicons/mod/RestartActivity.java
@@ -1,11 +1,19 @@
 package com.dr8.sbicons.mod;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import com.dr8.sbicons.R;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.stericson.RootTools.*;
+import com.stericson.RootTools.exceptions.RootDeniedException;
+import com.stericson.RootTools.execution.CommandCapture;
 
 public class RestartActivity extends Activity
 {
@@ -21,16 +29,27 @@ public class RestartActivity extends Activity
         .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				try {
-			        String command;
-			        String envp[] = new String[1];
-			        envp[0] = "PATH=" + System.getProperty("java.library.path");
-			        command = "pkill com.android.systemui";
-			        Process proc = Runtime.getRuntime().exec(new String[] { "su", "-c", command }, envp);
-			        proc.waitFor();
-			    } catch(Exception ex) {
-			        
-			    }				
+				if (RootTools.isBusyboxAvailable()) {
+					if (RootTools.isRootAvailable()) {
+						CommandCapture command = new CommandCapture(0, "pkill -TERM -f com.android.systemui");
+						try {
+							RootTools.getShell(true).add(command);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (TimeoutException e) {
+							Toast.makeText(RestartActivity.this, "Root request timed out", Toast.LENGTH_SHORT).show();
+						} catch (RootDeniedException e) {
+							Toast.makeText(RestartActivity.this, "Root request was denied", Toast.LENGTH_SHORT).show();
+						}
+						dialog.dismiss();
+						finish();
+					} else {
+						Toast.makeText(RestartActivity.this, "Your phone is not rooted", Toast.LENGTH_SHORT).show();
+					}
+				} else {
+					RootTools.offerBusyBox(RestartActivity.this);
+				}				
 			}
 		})
         .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
