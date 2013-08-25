63269014b12812c09e5491f490b0bf07df582a95
Dave W <drait@nebachadnezzar>
2013-08-25 16:47:20 -0700

    change charge animation + battery percent

--- ATTStatusbarIcons/src/com/dr8/sbicons/mod/hax/BatteryIcons.java
+++ ATTStatusbarIcons/src/com/dr8/sbicons/mod/hax/BatteryIcons.java
@@ -160,15 +160,13 @@ public class BatteryIcons {
 								String cbimg = "battery/charge/" + charge[i];
 								final Bitmap cb = ZipStuff.getBitmap(path, cbimg);
 								Drawable cd = new BitmapDrawable(null, cb);
-								if (paramPrefs.getBoolean("altcharge", false)) {
-									String bimg = "battery/" + battarray[blevel];
-									final Bitmap b = ZipStuff.getBitmap(path, bimg);
-									Drawable bd = new BitmapDrawable(null, b);
-									animation.addFrame(cd, 1000);
-									animation.addFrame(bd, 500);
-								} else {
-									animation.addFrame(cd, 1000);
-								}
+								animation.addFrame(cd, 1000);								
+							}
+							if (paramPrefs.getBoolean("altcharge", false)) {
+								String bimg = "battery/" + battarray[blevel];
+								final Bitmap b = ZipStuff.getBitmap(path, bimg);
+								Drawable bd = new BitmapDrawable(null, b);
+								animation.addFrame(bd, 500);
 							}
 							animation.setOneShot(false);
 							iv.setImageDrawable(animation);
